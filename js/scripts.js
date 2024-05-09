9870fa3cb4e80f874e15007f6a56be3c1a37d19f
Ana <bastosanaa11@gmail.com>
2024-05-09 11:38:35 -0300

    Update: Tratamento de erros nas entradas

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,30 @@ const descriptionElement = document.getElementById("description");
 const weatherIconElement = document.querySelector("#description-container img")
 const countryFlagElement = document.getElementById("country")
 const weatherDataContainer = document.querySelector("#weather-data")
+const errorMessage = document.getElementById("error-message")
 
 
 //implementando tratamento de erros de entrada
 const getWeatherData = async (city) => {
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
-    try{
+    try {
+        const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
         const response = await fetch(apiWeatherURL)
+
+        if (!response.ok) {
+            throw new Error('Erro ao buscar dados do clima')
+        }
+
         const data = await response.json()
         return data
     } catch (error) {
-        console.log("erro ao buscar clima", error)
+        console.log(error);
+        sendErrorMessage()
         return null
     }
 }
 const showWeatherData = async (city) => {
-    const data = await getWeatherData(city)
+        const data = await getWeatherData(city)
+        console.log(data)
     if (data) {
         cityNameElement.innerHTML = data.name
         temperatureElement.innerHTML = parseInt(data.main.temp)
@@ -39,6 +47,7 @@ const showWeatherData = async (city) => {
         const country = data.sys.country.toLowerCase()
         getCountryFlags(country)
         weatherDataContainer.classList.remove("hidden")
+        errorMessage.innerHTML = ""
     }
     
 }
@@ -51,6 +60,13 @@ const getCityPicture = async (city) => {
     const accessKey = "H948_LdWKr69x09IHlSXXN5hmTM-ql3Sf2pGt0BNkhA"
     const apiCityPicURL = `https://api.unsplash.com/photos/random/?query=${city}&client_id=${accessKey}`
 }
+
+const sendErrorMessage = () => {
+    errorMessage.innerHTML = "Ops - tente inserir outra cidade"
+    weatherDataContainer.classList.add("hidden")
+
+}
+
 
 
 searchBtn.addEventListener("click", (e) => {
